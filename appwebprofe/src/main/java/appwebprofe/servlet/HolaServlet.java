2cc8513f9e4a70cb60127846ae15bacf7cc5d97d
Val <valexx55@gmail.com>
2026-06-17 11:11:56 +0200

    servlets

---
+++ appwebprofe/src/main/java/appwebprofe/servlet/HolaServlet.java
@@ -0,0 +1,64 @@
+package appwebprofe.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+/**
+ * Servlet implementation class HolsServlet
+ */
+@WebServlet("/HolaServlet")
+public class HolaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HolaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    //http://localhost:8080/appwebprofe/HolaServlet GET
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//vamos a coger los valores de los parámetros nombre y edad
+		String nombreRecibido =  request.getParameter("nombre");
+		String edadRecibida =  request.getParameter("edad");
+		
+		if ((nombreRecibido!=null)&&(edadRecibida!=null)) {
+			
+			System.out.println("Nombre recibido = " + nombreRecibido);
+			System.out.println("Edad recibido = " + edadRecibida);
+			String mensaje = "Te llamas " + nombreRecibido +" y tienes " + edadRecibida +" años";
+			
+			response.getWriter().append(mensaje);
+			
+		} else {
+			//tengo que devolver 400
+			response.setStatus(400);
+			response.getWriter().append("Debes indicar parámetros nombre y edad");
+		}
+		
+		
+	}
+
+	//http://localhost:8080/appwebprofe/HolaServlet
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ basicosprofe/src/basicosprofe/dni/Dni.java
@@ -0,0 +1,51 @@
+package basicosprofe.dni;
+
+public class Dni {
+	
+	//secuencia de letras del DNI
+	//final - Constante
+	//static - A nivel de Clase - todos los Dnis comparten la misma SECUENCIA
+	 static final String SECUENCIA_LETRAS_DNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+	 //a nivel de objeto - "Dinámico" - No estático
+	 //cada Dni, tiene su propio número y letra
+	int numero;//integer número entero
+	char letra;//letra
+	
+
+	public Dni() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+	public Dni(int numero, char letra) {
+		//super();
+		this.numero = numero;
+		this.letra = letra;
+	}
+
+	
+
+	public Dni(int numero) {
+		super();
+		this.numero = numero;
+		this.letra = this.calcularLetra();
+	}
+
+	//TODO programr este métódo en Java, para que funcione bien y devuelva la letra correspondiente a cada de DNI
+	//calculándola como dice el algoritmo del Ministerio
+	public char calcularLetra() {
+		//como esta función devuelve un char, pues un char, letraDevuelta,
+		char letraDevuelta = ' ';//1ª línea, un objeto/variable del tipo devuelto
+		int resto = 0;	
+			
+			resto = this.numero % Dni.SECUENCIA_LETRAS_DNI.length();
+			letraDevuelta = Dni.SECUENCIA_LETRAS_DNI.charAt(resto);
+			
+		return letraDevuelta;//última línea, un return de la variable declarada en la línea 1
+	}
+	
+	
+	
+
+}
