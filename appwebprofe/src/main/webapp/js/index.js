0120e947144f7f4c149502897a312f0996960698
Val <valexx55@gmail.com>
2026-06-17 11:09:57 +0200

    subida Servlets

---
+++ appwebprofe/src/main/webapp/js/index.js
@@ -0,0 +1,8 @@
+//esto es un comentario
+//window es una variable
+//predefinida que representa la ventana del navegador
+//y alert, es una función de window, que permite
+//mostrar mensajes en formato pop up
+//window.alert("HOLA SOCIO"); // esto es un mensaje que sale por la ventana pop up    
+console.log("visitando la página");
+
