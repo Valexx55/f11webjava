ee194a272db5064c695a93ebac808549d4f1aa89
Val <valexx55@gmail.com>
2026-07-13 11:01:39 +0200

    interfaz

---
+++ opotestprofe/src/main/java/edu/xtd/opotestprofe/service/EnvioFacturaService.java
@@ -0,0 +1,13 @@
+package edu.xtd.opotestprofe.service;
+
+/*
+ * Nuestra interfaz: decimos qué hay que hacer, no cómo.
+ * Escribimos solamente la cabecera de los métodos:
+ * lo que recibo, lo que devuelvo y el nombre
+ * 
+ */
+
+public interface EnvioFacturaService {
+	
+	void enviarFactura(int idFactura);
+}
