700341642c6e2d09de92dc449a3a00819d17cf10
Val <valexx55@gmail.com>
2026-07-27 12:56:14 +0200

    ordendacion y lambdas

---
+++ basicosprofe/src/basicosprofe/cliente/Cliente.java
@@ -0,0 +1,9 @@
+package basicosprofe.cliente;
+
+public record Cliente(
+		long id,
+		String nombre,
+		String nif,
+		int edad) {
+
+}
---
+++ basicosprofe/src/basicosprofe/cliente/ComparadorClientes.java
@@ -0,0 +1,13 @@
+package basicosprofe.cliente;
+
+import java.util.Comparator;
+
+public class ComparadorClientes implements Comparator<Cliente>{
+
+	@Override
+	public int compare(Cliente cliente1, Cliente cliente2) {
+		
+		return cliente1.edad()-cliente2.edad();
+	}
+
+}
